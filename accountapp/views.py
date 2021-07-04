b41601e6d25d23d1fb3470bfc1a54e3d7ddd780e
seonjoo2030 <seonjoo1128@naver.com>
2021-07-04 13:32:16 +0000

    POST 방식의 동작 확인. 방식 생성, 텍스트 데이터 뿌리는 것 까지

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 
 def Hello_World(request):
 #    return HttpResponse("Hello World!")
-    return render(request, 'accountapp/hello_world.html') # 장고는 templates 경로를 지정해 놓으면, 앱 하위의 templates 경로도 인식한다.
+ #   return render(request, 'accountapp/hello_world.html') # 장고는 templates 경로를 지정해 놓으면, 앱 하위의 templates 경로도 인식한다.
 
+    if request.method == "POST":
+        return render(request, "accountapp/hello_world.html", context={'text': 'POST METHOD'})
+    else:
+        return render(request, "accountapp/hello_world.html")
 # Create your views here.
