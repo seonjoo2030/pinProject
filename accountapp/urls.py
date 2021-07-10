95c018869a9dbc14926aa8a51948497b4d519d18
seonjoo2030 <seonjoo1128@naver.com>
2021-07-10 06:57:05 +0000

    delete view 구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 
-from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, Hello_World
+from accountapp.views import AccountCreateView, AccountDeleteView, AccountDetailView, AccountUpdateView, Hello_World
 
 app_name = "accountapp"
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name="logout"),
     path('detail/<int:pk>', AccountDetailView.as_view(), name="detail"),
     path('update/<int:pk>', AccountUpdateView.as_view(), name="update"),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name="delete"),
 ]
