33b6103fd65d1e1920ff0c0a676ef6e5b2eed670
seonjoo2030 <seonjoo1128@naver.com>
2021-07-10 03:17:03 +0000

    models.py를 활용해서 db 접근, 사용 학습

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from accountapp.models import HelloWorld
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -7,7 +8,12 @@ def Hello_World(request):
  #   return render(request, 'accountapp/hello_world.html') # 장고는 templates 경로를 지정해 놓으면, 앱 하위의 templates 경로도 인식한다.
 
     if request.method == "POST":
-        return render(request, "accountapp/hello_world.html", context={'text': 'POST METHOD'})
+        temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, "accountapp/hello_world.html", context={'hello_world_output': new_hello_world})
     else:
         return render(request, "accountapp/hello_world.html")
 # Create your views here.
